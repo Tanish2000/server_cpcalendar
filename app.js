a7682de18569ec191e5eb23af8ef198ebc66b20e
Tanish Chouhan <chouhantanish@gmail.com>
2021-07-15 00:05:27 +0530

    Added user_data in API endpoint

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 const cron = require('node-cron');
+const ipInfo = require("ipinfo");
+const { getClientIp } = require("@supercharge/request-ip");
 
 
 
@@ -24,7 +26,7 @@ const Contest = require('./model/contestSchema');
 const PORT = process.env.PORT || 5000;
 
 var corsOptions = {
-    origin: ['https://cpcalendar.netlify.app','http://localhost:3000'],
+    origin: ['https://cpcalendar.netlify.app', 'http://localhost:3000'],
     optionsSuccessStatus: 200
 }
 
@@ -38,13 +40,17 @@ app.use(cors(corsOptions));
 app.get('/getContestData', async (req, res) => {
 
     try {
+        const user_ip = getClientIp(req);
+        const location = await ipInfo(user_ip);
         const response = await Contest.find();
         console.log('Data fetched sucessfully');
-        
+
         return res.status(200).json({
-             "status": 200,
-             "total_contests" : response.length,
-             "contests": response })
+            "status": 200,
+            "total_contests": response.length,
+            "contests": response,
+            "user_data" : location
+        })
 
     } catch (err) {
         return res.status(err.code).json({ "error": err.message });
