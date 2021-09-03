154b93ae64ac9b48f72e23c4a15efd56e8d4e788
Tanish Chouhan <chouhantanish@gmail.com>
2021-09-03 23:55:00 +0530

    Scheduled Email Notifications

--- app.js
+++ app.js
@@ -9,61 +9,52 @@ const ipInfo = require("ipinfo");
 const { getClientIp } = require("@supercharge/request-ip");
 const TodayContest = require('./mailer/todayContest');
 
-
-
 dotenv.config({ path: path.resolve(__dirname, './config.env') });
 
 require('./db/connection');
 
-require("./mailer/mailer");
+const mailer = require("./mailer/mailer");
+cron.schedule('5 * * * *', ()=> {
+    console.log("Emails.")
+    mailer();
+})
 
 const updateContestData = require('./scrapper/scheduler');
-
 cron.schedule('*/60 * * * *', () => {
     updateContestData();
 })
 
 const Contest = require('./model/contestSchema');
-
-
 const PORT = process.env.PORT || 5000;
-
 var corsOptions = {
     origin: ['https://cpcalendar.netlify.app', 'http://localhost:3000'],
     optionsSuccessStatus: 200
 }
 
-
 //Middlewares
 app.use(helmet());
 app.use(cors(corsOptions));
 
-
-
 app.get('/getContestData', async (req, res) => {
-
     try {
         const user_ip = getClientIp(req);
         const location = await ipInfo(user_ip);
         const response = await Contest.find();
         const todaycontest = await TodayContest();
         console.log('Data fetched sucessfully');
-
         return res.status(200).json({
             "status": 200,
             "total_contests": response.length,
+            "contests_today" : todaycontest.length,
             "contests": response,
             "today_contest" : todaycontest,
             "user_data" : location
         })
-
     } catch (err) {
         return res.status(err.code).json({ "error": err.message });
     }
-
 })
 
 app.listen(PORT, () => {
     console.log("Server running on PORT:", PORT);
 })
-
--- mailer/mailer.js
+++ mailer/mailer.js
@@ -2,69 +2,38 @@ const nodemailer = require('nodemailer');
 const Email = require('email-templates');
 const today_contest = require('./todayContest')
 
+const notification_mailer = () => {
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.MAIL,
-    pass: process.env.MAIL_PASS,
-  }
-});
-
-const email = new Email({
-  transport: transporter,
-  send: true,
-  preview: true,
-});
-
-today_contest().then(res => {
-  if (res.length) {
-    console.log(res)
-    email.send({
-      template: 'contest',
-      message: {
-        to: ['hopeplasmaofficial@gmail.com' ,'vinayaks0031@gmail.com']
-      },
-     locals : {
-        contests : [
-          {
-          "_id": 5,
-          "platform": "Codeforces",
-          "hex_color": "#212529",
-          "link": "https://codeforces.com/contest/1562",
-          "title": "Codeforces Round #741 (Div. 2)",
-          "start": "26 Aug 2021",
-          "end": "26 Aug 2021",
-          "start_time": "8:05 PM",
-          "end_time": "10:05 PM",
-          "__v": 0
-          },
-          {
-          "_id": 1,
-          "platform": "Codechef",
-          "title": "August Lunchtime 2021",
-          "start": "28 Aug 2021",
-          "end": "28 Aug 2021",
-          "start_time": "7:30 PM",
-          "end_time": "10:30 PM",
-          "hex_color": "#A0522D",
-          "link": "https://www.codechef.com/LTIME99?itm_campaign=contest_listing",
-          "__v": 0
-          },
-          {
-          "_id": 9,
-          "platform": "Leetcode",
-          "title": "Weekly Contest 256",
-          "start": "29 Aug 2021",
-          "end": "29 Aug 2021",
-          "start_time": "1:30 PM",
-          "end_time": "3:00 PM",
-          "link": "https://leetcode.com/contest/weekly-contest-256",
-          "hex_color": "#edaf05",
-          "__v": 0
-          }], 
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.MAIL_PASS,
     }
-    }).then(() => console.log('email has been sent!'));
-  }
-}).catch(err => console.log(err.message))
+  });
+  
+  const email = new Email({
+    transport: transporter,
+    send: true,
+    preview: true,
+  });
+  
+  today_contest().then(res => {
+    if (res.length) {
+      console.log("Sending Emails..........")
+      email.send({
+        template: 'contest',
+        message: {
+          bcc : ['hopeplasmaofficial@gmail.com' , 'chouhantanish@gmail.com' ,'vinayaks0031@gmail.com']
+        },
+       locals : {
+          contests : res, 
+      }
+      }).then(() => console.log('Emails has been sent!'));
+    }
+  }).catch(err => console.log(err.message))
+
+}
+
+module.exports = notification_mailer;
 
