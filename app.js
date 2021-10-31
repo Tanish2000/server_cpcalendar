3aaa1cd4f783d58ca57760c7967e217a76c7447b
Tanish Chouhan <chouhantanish@gmail.com>
2021-10-31 18:56:59 +0530

    Added API endpoint for subscription list

--- app.js
+++ app.js
@@ -7,17 +7,13 @@ const path = require('path');
 const cron = require('node-cron');
 const ipInfo = require("ipinfo");
 const { getClientIp } = require("@supercharge/request-ip");
-// const TodayContest = require('./mailer/todayContest');
+const TodayContest = require('./mailer/todayContest');
+const User = require('./model/userSchema');
 
 dotenv.config({ path: path.resolve(__dirname, './config.env') }); //including the .env variables
 
 require('./db/connection'); //making connection with Database
 
-// const mailer = require("./mailer/mailer"); //importing mailer and scheduling
-// cron.schedule('57 23 * * *', ()=> {
-//     console.log("Sending emails....")
-//     mailer();
-// })
 
 const updateContestData = require('./scrapper/scheduler'); //importing scrapper and scheduler
 cron.schedule('*/60 * * * *', () => {
@@ -34,6 +30,7 @@ var corsOptions = {  //cors settings
 //Middlewares
 app.use(helmet());
 app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get('/getContestData', async (req, res) => { //API endpoint to fetch contest data
     try {
@@ -45,14 +42,39 @@ app.get('/getContestData', async (req, res) => { //API endpoint to fetch contest
         return res.status(200).json({   //returning contests data
             "status": 200,
             "total_contests": response.length,
-            "contests_today" : todaycontest.length,
+            "contests_today": todaycontest.length,
             "contests": response,
-            "today_contest" : todaycontest,
-            "user_data" : location
+            "today_contest": todaycontest,
+            "user_data": location
         })
     } catch (err) {
         //handling errors
         return res.status(err.code).json({ "error": err.message });
+    }
+})
+
+app.post('/addUser', async (req, res) => {
+    try {
+        const { email } = req.body;
+        const UserExist =  await User.findOne({ email : email  })
+        if(UserExist) // If user already exists
+        {
+            return res.status(403).json({ status : 403 , message : " 🛑 You are already there!"})
+        }
+        else { // for new users 
+            const new_user = new User({ email })
+            const response  = await new_user.save();
+            if(response) {
+                return res.status(200).json({ status : 200 , message : "🎉 Added! Sit back and Relax."})
+            }
+            else {
+                return res.status(500).json({ status : 500 , message : "Internal Server Error."})
+            }            
+        }
+    }
+    catch(err) {
+        console.log(err)
+        return res.status(500).json({ status : 500 , message : "Internal Server Error." })
     }
 })
 
