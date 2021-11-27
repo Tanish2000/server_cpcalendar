724da3be59d13c948980d57640ec1d0109426d1f
Tanish Chouhan <chouhantanish@gmail.com>
2021-11-27 21:41:59 +0530

    Updated Cors policy

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ cron.schedule('*/60 * * * *', () => {
 const Contest = require('./model/contestSchema');
 const PORT = process.env.PORT || 5000;
 var corsOptions = {  //cors settings
-    origin: ['*'],
+    origin: '*',
     optionsSuccessStatus: 200
 }
 
