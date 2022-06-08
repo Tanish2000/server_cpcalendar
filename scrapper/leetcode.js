c41a124615afcf1026c91e3854ece1af8d320473
Tanish <tanish@ceegees.in>
2022-06-08 15:56:35 +0530

    Updated scrapping process for Leetcode Contests

--- scrapper/leetcode.js
+++ scrapper/leetcode.js
@@ -1,57 +1,43 @@
-const puppeter = require('puppeteer');
-const FormatDate = require('../utility/FormatDates');
-const SetEndtime = require("../utility/SetEndtime");
+const FormatTime = require('../utility/FormatTime');
+const axios = require('axios');
+
+const getDate = (date) => {
+    let date_str = new Date(date);
+    let date_arr = date_str.toDateString().split(' ');
+    return date_arr[2] +" " + date_arr[1] + " "  + date_arr[3];
+}
 
 const getLeetcodeData = async () => {
+
     try {
-
-        var browser = await puppeter.launch({ headless: true, args: ['--no-sandbox'] }); //initializing puppeter
-        var page = await browser.newPage(); //new browser page on chromium
-
-        await page.goto('https://leetcode.com/contest/', { waitUntil: 'load', timeout: 0 }); //opening the contest page in the browser
-        await page.waitForSelector("#contest-app > div > div > div.container > div.contest-cards-base > div > div") //waiting for the div to load
-
-        var contests = await page.evaluate(() => {  //extracting contest details from the page
-            var result = [];
-            var htmlDOM = $(document.querySelectorAll('#contest-app > div > div > div.container > div.contest-cards-base > div > div')).children();  //div that holds contest data
-            htmlDOM = htmlDOM.toArray(); //converting DOM to Array 
-            htmlDOM.forEach((ele) => {
-                var temp = {};
-                //extrating all the details for a particular contest
-                var contest_data = ele.children[0];
-                temp["platform"] = "Leetcode";
-                temp["title"] = contest_data.querySelector('.card-title').innerText;
-                var contest_details = contest_data.querySelector('.time').innerText;
-                temp["start"] = contest_details.split('@')[0].trim();
-                temp["end"] = temp["start"];
-                var time = contest_details.split('@')[1].trim().slice(0, 17).split('-');
-                temp["start_time"] = time[0].trim();
-                temp["end_time"] = time[1].trim();
-                temp['link'] = "https://leetcode.com/contest/" + temp["title"].trim().replaceAll(" ", "-").toLowerCase();
-                temp["hex_color"] = "#edaf05";
-                result.push(temp); //pushing object into the result array 
-            })
-            return result;
-        })
-        contests.forEach(ele => { //formating date and time  
-            ele["end"] = ele["start"] = FormatDate(ele["start"]);
-            ele["start_time"] = SetEndtime(ele["start_time"], "05:30");
-            ele["end_time"] = SetEndtime(ele["end_time"], "05:30");
-        })
-
-        //object that holds the scraped data
-        const final_result = {
-            "status": 200,
-            "leetcode_contests": contests
-        }
-        await browser.close(); //closing the browser
-        return final_result;
-    } catch (error) {
-        //handling errors
+        const fetched_data = await axios.get('https://kontests.net/api/v1/leet_code');
+        const response = fetched_data.data;
+        
+        const result = response.map((ele) => {
+            const temp_obj = {};
+            temp_obj.platform = "Leetcode";
+            temp_obj.title = ele.name;
+            temp_obj.hex_color = "#edaf05";
+            temp_obj.link = "https://leetcode.com/contest/" + ele.name.trim().replaceAll(" ", "-").toLowerCase(); 
+            temp_obj.start = getDate(ele.start_time);
+            temp_obj.end = getDate(ele.end_time);  
+            temp_obj.start_time = FormatTime(ele.start_time);
+            temp_obj.end_time = FormatTime(ele.end_time);
+            return temp_obj;
+        })       
         return {
-            "status": 500,
-            "error": error.message
-        };
+            "status" : 200,
+            "leetcode_contests" : result
+        }
     }
-};
+    catch(err) 
+    {
+        return {
+            "status" : 500,
+            "error" : err.message
+        }
+    }
+    
+}
+
 module.exports = getLeetcodeData;
